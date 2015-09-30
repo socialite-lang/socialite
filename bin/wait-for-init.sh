03bc6246db81726d20171cde567f89609163ae27
Jiwon Seo <seojiwon@gmail.com>
2015-09-30 11:31:07 -0700

    Removed old scripts

--- bin/wait-for-init.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-bin=`dirname "$0"`
-bin=`cd "$bin"; pwd`
-
-. "$bin/socialite-config.sh"
-
-sh -c 'tail -n +1 -F ../logs/socialite-master*.log | { sed "/Ready/ q" && kill $$ ;}'
