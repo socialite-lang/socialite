607fa28c0638242646274ace752945e69c3aef24
Jiwon Seo <seojiwon@gmail.com>
2016-07-26 14:02:45 +0900

    Added flexible partitioning (with shardby)

--- test/test_all.py
+++ test/test_all.py
@@ -1,14 +1,5 @@
 import unittest
 
-import test_functions
-import test_tables
-import test_joins
-
-alltests = [test_functions, test_tables, test_joins]
-
-def main():
-    for t in alltests:
-        t.main()
-
 if __name__ == '__main__':
-    unittest.main()
+    testsuite = unittest.TestLoader().discover(".", pattern="test_*.py")
+    unittest.TextTestRunner(verbosity=1).run(testsuite)
--- test/test_shard.py
+++ test/test_shard.py
@@ -8,9 +8,9 @@ import unittest
 class TestBase(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        s.run("Bar(int a:0..1000, float b).")
-        s.run("Qux(int a:0..2000, float b) groupby(1).")
-        s.run("Sum(int i:0..0, float sum) groupby(1).")
+        s.run("Bar(int a:0..1000, int b).")
+        s.run("Qux(int a:0..2000, int b) groupby(1).")
+        s.run("Sum(int i:0..0, int sum) groupby(1).")
         cls.setUpMoreClass()
         cls.initTables()
 
@@ -25,63 +25,59 @@ class TestBase(unittest.TestCase):
         s.run("Foo(a,b) :- a=$range(30, 40), b=$range(1000, 2000).")
         s.run("Foo(a,b) :- a=$range(40, 50), b=$range(0, 1000).")
 
-        s.run("Bar(a,b) :- a=$range(10,20), b=11.0f.")
-        s.run("Bar(a,b) :- a=$range(30,40), b=37.0f.")
-        s.run("Bar(a,b) :- a=$range(40,50), b=53.0f.")
-        s.run("Bar(a,b) :- a=$range(50,100), b=97.0f.")
+        s.run("Bar(a,b) :- a=$range(10,20), b=a+11.")
+        s.run("Bar(a,b) :- a=$range(30,40), b=a+37.")
+        s.run("Bar(a,b) :- a=$range(40,50), b=a+53.")
+        s.run("Bar(a,b) :- a=$range(50,100), b=a+97.")
 
     @classmethod
     def tearDownClass(cls):
         s.drop("*")
+
+    def setUp(self):
+        s.clear("Qux")
+        s.clear("Sum")
 
     def run_simple_rule(self):
         s.run("Qux(b, $sum(f)) :- Bar(a, f), Foo(a, b).")
         s.run("Sum(0, $sum(f)) :- Qux(b, f).")
 
         _, _sum = s.query("Sum(0, f)").first()
-        xsum=0.0
-        for b,f in s.query("Qux(b,f)"):
-            xsum+=f
-        print "xsum:", xsum, "sum:", _sum
 
-
-        self.assertEquals(_sum, xsum)
-        #self.assertEquals(_sum, 1945000.0)
+        self.assertEquals(_sum, 1945000.0)
 
 class TestShardByCol0(TestBase):
     @classmethod
     def setUpMoreClass(cls):
         s.run("Foo(int a:0..1000, (int b)).")
 
-    def setUp(self):
-        s.clear("Qux")
-        s.clear("Sum")
-
     def test_simple(self):
-        s.run("Qux(b, f) :- b=$range(0, 2000), f=b+131.2f.")
-        s.run("Sum(0, $sum(f)) :- Qux(b,f).")
-        _, _sum = s.query("Sum(0, f)").first()
-        xsum=0.0
-        for b,f in s.query("Qux(b,f)"):
-            xsum+=f
-        print "xsum:", xsum, "sum:", _sum
+        self.run_simple_rule()
 
-        self.assertEquals(_sum, xsum)
- 
-@unittest.skip("")
+
+class TestShardByCol0Sorted(TestBase):
+    @classmethod
+    def setUpMoreClass(cls):
+        s.run("Foo(int a:0..1000, (int b)) sortby b.")
+        
+    def test_simple(self):
+        self.run_simple_rule()
+
 class TestShardByCol1(TestBase):
     @classmethod
     def setUpMoreClass(cls):
         s.run("Foo(int a:0..1000, (int b)) shardby b.")
 
-    def setUp(self):
-        s.clear("Qux")
-        s.clear("Sum")
+    def test_simple(self):
+        self.run_simple_rule()
+
+class TestShardByCol1Sorted(TestBase):
+    @classmethod
+    def setUpMoreClass(cls):
+        s.run("Foo(int a:0..1000, (int b)) shardby b, sortby b.")
 
     def test_simple(self):
-        for i in range(50):
-            self.run_simple_rule()
-            self.setUp()
+        self.run_simple_rule()
 
 
 def main():
